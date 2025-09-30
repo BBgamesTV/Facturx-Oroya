0dabd0ebdf842dbec11133d3bfe2bdcc881470f7
souksana <souksana@oroya.fr>
2025-09-30 15:42:30 +0200

    readme.md

--- src/Service/FacturxService.php
+++ src/Service/FacturxService.php
@@ -8,19 +8,17 @@ class FacturxService
     public function generateFacturxPdf(string $xmlPath, string $outputPath, string $pdfContent): void
     {
         $xmlContent = file_get_contents($xmlPath);
-
-        // Ici on utilise Writer
+        
         $writer = new Writer();
 
-        // generate(string $pdfInvoice, string $xml, ?string $profile = null, bool $validateXSD = true, array $additionalAttachments = [], bool $addLogo = false, string $relationship = 'Data')
         $facturxPdf = $writer->generate(
-            $pdfContent,      // ton PDF (string binaire)
-            $xmlContent,      // ton XML Factur-X
-            null,             // profil (auto-détection si null)
-            false,             // validation XSD
-            [],               // pièces jointes additionnelles
-            false,            // ajouter logo Factur-X en haut de la 1ère page
-            'Data'            // relation
+            $pdfContent,      
+            $xmlContent,      
+            null,             
+            false,             
+            [],               
+            false,            
+            'Data'            
         );
 
         file_put_contents($outputPath, $facturxPdf);
